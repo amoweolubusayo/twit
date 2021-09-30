dd510af4029e93e127eb6ad5499fd0ef5d6d901a
Olubusayo Amowe <amoweolubusayo@gmail.com>
2021-09-30 16:11:14 +0100

    login,register components

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
@@ -11,6 +11,19 @@ import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { ExploreComponent } from './explore/explore.component';
+import { PostsComponent } from './posts/posts.component';
+import { PersonalPostsComponent } from './personalposts/personalposts.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SharedService } from './shared/shared.service';
+import { RegisterModel, ExploreModel, LoginModel } from './shared/shared.model';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatSidenavModule,MatCardModule, MatFormFieldModule, MatIconModule } from '@angular/material';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+
+
 
 @NgModule({
   declarations: [
@@ -20,21 +33,38 @@ import { LoginComponent } from './login/login.component';
     CounterComponent,
     FetchDataComponent,
     RegisterComponent,
-    LoginComponent
+    LoginComponent,
+    ExploreComponent,
+    PostsComponent,
+    PersonalPostsComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
       { path: 'register', component: RegisterComponent },
       { path: 'login', component: LoginComponent },
-    ])
+      { path: 'explore', component: ExploreComponent },
+      { path: 'post', component: PostsComponent},
+      { path: 'personalpost', component: PersonalPostsComponent},
+      { path: 'profile', component: ProfileComponent}
+    ]),
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatSnackBarModule
   ],
-  providers: [],
+  providers: [SharedService,RegisterModel,ExploreModel,LoginModel],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- ClientApp/src/app/register/register.component.ts
+++ ClientApp/src/app/register/register.component.ts
@@ -1,15 +1,49 @@
-import { Component } from '@angular/core';
-//import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NgForm } from '@angular/forms';
+import { SharedService } from '../shared/shared.service';
+import { RegisterModel, RegisterSuccessModel } from '../shared/shared.model';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-register-component',
-  templateUrl: './register.component.html'
+  templateUrl: './register.component.html',
 })
-export class RegisterComponent {
-//   registerForm: FormGroup;
-//   constructor(
-//     private formBuilder: FormBuilder,
-// ){
-// }
+
+export class RegisterComponent implements OnInit {
+  public reg
+  durationInSeconds = 10;
+  constructor(
+    public service: SharedService,
+    public model : RegisterModel,
+    public _snackBar: MatSnackBar,
+    public router : Router)
+    {
+  }
+  ngOnInit(): void {
+    }
+    onSubmit(form: NgForm) {
+      if(form.valid){
+      this.service.register(form.value)
+        .subscribe((data: RegisterModel[]) => {
+          this.reg = data;
+          console.log("let's see: "+this.reg.message);
+        this._snackBar.open(this.reg.message,'Close', {
+          duration: this.durationInSeconds * 1000,
+        });
+        this.router.navigate(["/login"]);
+      form.reset();
+    },
+    error => {
+      this._snackBar.open('User already exists','Close',{
+        duration: this.durationInSeconds * 1000
+      });
+      form.reset();
+    });
+    }
+      console.log(form.value);
+    }
 }
+
